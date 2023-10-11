5ceeb7f38ff47183ab428c73c2330551e503c11e
reksher <stanislav-sannicov@mail.ru>
2023-10-11 16:50:23 +0400

    add assertion error

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,20 @@
 from selenium.webdriver.common.by import By
 from .base_page import BasePage
+from selenium.common.exceptions import NoSuchElementException
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
 
