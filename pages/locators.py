b34c8e7b5f9ff7a1ded4717e55e0a62bf1ad2075
reksher <stanislav-sannicov@mail.ru>
2023-10-23 21:59:57 +0400

    code style in page object and add tests for product page

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+
 class LoginPageLocators():
     email_reg = (By.CSS_SELECTOR, '[id="id_registration-email"]')
     password1_reg = (By.CSS_SELECTOR, '[id="id_registration-password1"]')
@@ -13,3 +14,12 @@ class LoginPageLocators():
     password_log = (By.CSS_SELECTOR, '[id="id_login-password"]')
     button_log = (By.CSS_SELECTOR, '[name="login_submit"]')
 
+
+class PromoPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    NOTIFICATION_OF_ADD = (By.CSS_SELECTOR, 'div.alert:first-child .alertinner')
+    NOTIFICATION_OF_ADD_NAME_BOOK = (By.CSS_SELECTOR, 'div.alert:first-child strong')
+    NAME_BOOK = (By.CSS_SELECTOR, 'div.col-sm-6.product_main > h1')
+    NOTIFICATION_PRICE = (By.CSS_SELECTOR, 'div.alert:last-child .alertinner p:first-of-type')
+    NOTIFICATION_PRICE_IN_BASKET = (By.CSS_SELECTOR, 'div.alert:last-child strong')
+    PRICE_BOOK = (By.CSS_SELECTOR, '.col-sm-6 .price_color')
---
+++ pages/product_page.py
@@ -0,0 +1,33 @@
+from pages.base_page import BasePage
+from pages.locators import PromoPageLocators
+
+
+class PromoPage(BasePage):
+    def add_to_basket_promo_full_tests(self):
+        #self.should_be_url() #Вызываем метод для проверки корректности URL
+        add_basket = self.browser.find_element(*PromoPageLocators.ADD_TO_BASKET)
+        add_basket.click()  # Действие, клик по кнопке "добавить в корзину"
+        self.solve_quiz_and_get_code() #Вызов математической операции из класса BasePage(по заданию дано)
+        self.should_be_correct_notification() #Вызов метода для проверки текста уведомления о добавлении в корзину
+        self.should_be_price_notification() #Вызов метода для проверки текста "стоимость корзины теперь..."
+        self.should_be_correct_notification_for_price() #Вызов метода для проверки самой стоимости цены
+
+    #def should_be_url(self):
+        #assert '?promo=newYear' in self.browser.current_url, 'newYear missing from url'
+
+    def should_be_correct_notification(self):
+        assert 'был добавлен в вашу корзину.' in self.browser.find_element(*PromoPageLocators.NOTIFICATION_OF_ADD).text, f'Notification of adding to the cart is incorrect, actual result {self.browser.find_element(*PromoPageLocators.NOTIFICATION_OF_ADD).text}'
+        # Проверка на корректность текста нотификации о добавлении в корзину
+        message = self.browser.find_element(*PromoPageLocators.NOTIFICATION_OF_ADD_NAME_BOOK).text
+        #Дергаем и переводим в текст нужное имя книги в сообщении о добавлении в корзину
+        product_name = self.browser.find_element(*PromoPageLocators.NAME_BOOK).text
+        #Дергаем и переведим в текст нужное имя книги на странице товара
+        assert message == product_name, 'Name in message != name in pages'
+
+    def should_be_price_notification(self):
+        assert 'Стоимость корзины теперь составляет' in self.browser.find_element(*PromoPageLocators.NOTIFICATION_PRICE).text, (f'Notification indicating the cost is incorrect, actual result {self.browser.find_element(*PromoPageLocators.NOTIFICATION_PRICE).text}')
+        # Проверка, что текст нотификации корректен
+
+    def should_be_correct_notification_for_price(self):
+        assert self.browser.find_element(*PromoPageLocators.PRICE_BOOK).text in self.browser.find_element(*PromoPageLocators.NOTIFICATION_PRICE_IN_BASKET).text, f'The price in the notification is incorrect, actual result {self.browser.find_element(*PromoPageLocators.NOTIFICATION_PRICE_IN_BASKET).text}'
+        #Проверка, что стоимость товара равна стоимости в нотификации
